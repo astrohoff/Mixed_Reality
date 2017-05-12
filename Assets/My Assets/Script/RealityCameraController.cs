d5b74169c4dde1c6aab2e90567c74d02fd1f455c
Alec Hoffmann <astrohoff@ufl.edu>
2017-05-12 13:39:12 -0400

    Added non-vr camera behavior. Added webcam feed. Added spheres for testing.

---
+++ Assets/My Assets/Script/RealityCameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class RealityCameraController : MonoBehaviour {
+	public Camera cam;
+	public MeshRenderer webcamFeed;
+
+	// Use this for initialization
+	void Start () {
+		if (!VRSettings.isDeviceActive && WebCamTexture.devices.Length > 0) {
+			cam.enabled = true;
+			cam.aspect = (4f / 3);
+			Material webcamMat = new Material ("Unlit/Transparent Cutout");
+			webcamFeed.material = webcamMat;
+			webcamMat.mainTexture = new WebCamTexture ();
+		} else {
+			cam.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
