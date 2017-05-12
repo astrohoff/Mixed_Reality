769bd012a8b39c11de721b9b940f7790a92c7893
Alec Hoffmann <astrohoff@ufl.edu>
2017-05-12 12:24:56 -0400

    Added object grabbing. Added PUN random room joining. Made VR stuff only spawn if there is a VR device.

---
+++ Assets/My Assets/Script/PlayerSpawnController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class PlayerSpawnController : MonoBehaviour {
+	public GameObject vrPlayerPrefab;
+
+	void Start(){
+		if (VRSettings.isDeviceActive) {
+			Instantiate (vrPlayerPrefab, Vector3.zero, Quaternion.identity);
+		}
+	}
+}
